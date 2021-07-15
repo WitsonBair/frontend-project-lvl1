e8e9d92c7f2b28601e1b67690733f7355e8c9b45
witsonbair <tsyden2.bair@gmail.com>
2021-07-16 02:20:18 +0800

    5th step 2

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { enquireName, getRandom } from '../cli.js';
-import { engine } from '../game-engine.js';
+import { enquireName, getRandom } from '../src/cli.js';
+import { engine } from '../src/game-engine.js';
 
 const quest1 = 'Answer "yes" if the number is even, otherwise answer "no".';
 const qntt_of_arg = 1;
